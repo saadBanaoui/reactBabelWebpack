1251ba314271a38b9a46d99d011c8c5a7acac236
Saad <se@kapt.mobi>
2017-08-22 10:41:22 +0200

    commit

--- webpack.config.js
+++ webpack.config.js
@@ -20,15 +20,21 @@ module.exports = {
             { test: /\.js$/, loader: 'babel-loader', exclude: /node_modules/ },
             { test: /\.jsx$/, loader: 'babel-loader', exclude: /node_modules/ },
             {
+                test: /\.css/,
+                loader: 'style-loader!css-loader!autoprefixer-loader'
+            },
+            {
                 test: /\.scss$/,
-                use: [{
+                use: [
+                    {
                     loader: "style-loader" // creates style nodes from JS strings
                     }, {
                     loader: "css-loader" // translates CSS into CommonJS
                     }, {
                     loader: "sass-loader" // compiles Sass to CSS
-                }]
-            } 
+                    }
+                ]
+            }
         ]
     },
     plugins: [HtmlWebpackPluginConfig]
